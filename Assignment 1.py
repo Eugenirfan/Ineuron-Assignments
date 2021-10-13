1ff10e0fb0a503037d5c676e69f9fb7920962d3d
Eugenirfan <46944817+Eugenirfan@users.noreply.github.com>
2021-10-13 07:07:33 -0400

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,12 @@
+y= [x for x in range(2000,3201) if x%7==0 and x%5!=0]
+
+print(y)
+
+f_name=input("Enter you First name")
+l_name=input("Enter you Last name")
+print (f'{l_name} {f_name}'[::-1])
+
+diameter=12
+pi=3.142
+volume_sphere=  4/3*pi*((diameter/2)**3)
+print(volume_sphere)
